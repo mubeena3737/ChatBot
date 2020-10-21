7211b9be52c60d6855a1655d8fa24c7e2bb90265
mubeena3737 <72685730+mubeena3737@users.noreply.github.com>
2020-10-21 22:41:43 +0530

    Create Bsc.py

---
+++ Bsc.py
@@ -0,0 +1,7 @@
+def Bsc():
+    print('this is a 3 years degree ')
+    print('the groups  are ...')
+    sub=['1.maths,  chemistry,  physics\t','2.com.science,  maths,  physics\t','3.maths  ,physics,  electronics\t','4.1maths,  statics,  com.sciencs']
+    for i in sub:
+        print(i)
+    return 
