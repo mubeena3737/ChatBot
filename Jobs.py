34fe8c65e5dee1d9136fde2de9c42ba74aa4c8e9
mubeena3737 <72685730+mubeena3737@users.noreply.github.com>
2020-10-21 22:43:03 +0530

    Create Jobs.py

---
+++ Jobs.py
@@ -0,0 +1,9 @@
+def Jobs():
+    print('please tell us jobs via B.Tech or B.Sc...')
+    print('1.B.Tech, 2. B.Sc')
+    job_choosen=int(input())
+    if job_choosen==1:
+        print("Civils,Groups,IES,IFS,Defence,Railways,Banks,AEE,PSUs,IT,Pharma,cementy,Automobile,Real Estate....")
+    else:
+        print("Civils,Groups,Defence,IFS,Banks,Computers,Retail,Media,Entertainment...")
+    return
