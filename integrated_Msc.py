b9dc013cdb7fc790cc2c9065a792cc92f0240c7b
mubeena3737 <72685730+mubeena3737@users.noreply.github.com>
2020-10-21 22:42:13 +0530

    Create integrated_Msc.py

---
+++ integrated_Msc.py
@@ -0,0 +1,4 @@
+def integrated_Msc():
+    print('It is a 5 years degree ')
+    print('here the btech and mtech are done n five years and the students posess jobs or go on with Ph.D .')
+    return
