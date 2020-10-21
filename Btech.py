b44d7c4e4df68bc55f3d18a7d7fe642f0a927d97
mubeena3737 <72685730+mubeena3737@users.noreply.github.com>
2020-10-21 22:40:26 +0530

    Create Btech.py

---
+++ Btech.py
@@ -0,0 +1,8 @@
+def  Btech():
+    print("Normally it is a four ye B.l()ars degree.")
+    print("most of the students will choose thes groups like ")
+    groups=['CSE','ECE','MECH','CIVIL','EEE','IT']
+    for i in groups:
+        print(i)
+    print('..........nextly after this u can do job or do further high studies')
+    return 
